e8134b649270795a60bcb682c9002d510b51147d
Narayan D <happycow@gmail.com>
2020-01-17 21:12:17 -0500

    v2 with code refactored using filter and forEach methods

--- JS101/lesson4/frequencyLetters.js
+++ JS101/lesson4/frequencyLetters.js
@@ -1,8 +1,16 @@
 //create an object that expresses the freq with with each letter occurs in a
 //string
 
-let statement = "The Flintsones Rock";
+let statement = "The Flintstones Rock";
 let charFrequency = {};
+let charFrequency2 = {};
+
+let characters = statement.split('').filter(char => char !== " ");
+
+characters.forEach(char => {
+  charFrequency2[char] = charFrequency2[char] + 1 || 1;
+  //charFrequency[char] += 1;
+});
 
 for (let ctr = 0; ctr < statement.length; ctr += 1) {
   let freq = 0;
@@ -17,4 +25,5 @@ for (let ctr = 0; ctr < statement.length; ctr += 1) {
 }
 
 console.log(charFrequency);
+console.log(charFrequency2);
 
