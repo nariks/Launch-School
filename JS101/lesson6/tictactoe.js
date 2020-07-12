623d1bad34c47d5f3828f030e7542ccaebe749b9
Narayan D <happycow@gmail.com>
2020-07-12 09:49:09 -0400

    created a generalised board

--- JS101/lesson6/tictactoe.js
+++ JS101/lesson6/tictactoe.js
@@ -1,28 +1,43 @@
 const BOARD_ROWS = 3;
 const BOARD_COLS = 3;
-const SQUARE_ROWS = 3;
-const SQUARE_COLS = 3;
+const SQUARE_ROWS = 3;      //Square rows must be odd;
+const SQUARE_COLS = 5;
 const MAX_SQUARES = BOARD_ROWS * BOARD_COLS;
 const CTR_MAX = MAX_SQUARES + (SQUARE_ROWS - 1);
+const SPACE_FILLER = Array(SQUARE_COLS).fill(' ').join('');
+const SEPARATOR = Array(SQUARE_COLS).fill('-').join('');
+
+function displayPositions(board, row) {
+  let start = row * BOARD_COLS;
+  let end = (row * BOARD_COLS) + BOARD_COLS;
+  let positions = board.slice(start, end);
+  positions = positions.map( position => `  ${position}  `);
+  positionRow = positions.join('|');
+  console.log(positionRow);
+}
+
+function displaySquares(board, row){
+  for(let sqRow = 0; sqRow < SQUARE_ROWS; sqRow += 1) {
+    if(sqRow === (SQUARE_ROWS - 1) / 2) {
+      displayPositions(board, row);
+      continue;
+    }
+    console.log(Array(BOARD_COLS).fill(SPACE_FILLER).join('|'));
+  }
+}
 
 function displayBoard(board) {
-  for(let brdRowCtr = 0; brdRowCtr < BOARD_ROWS; brdRowCtr += 1) {
-    for (let sqRowCtr = 0; sqRowCtr < SQUARE_ROWS; sqRowCtr += 1) {
-      if(sqRowCtr % 2 === 0) {
-        console.log('     |     |');
-        continue;
-      }
-      let sq_pos = brdRowCtr * BOARD_COLS;
-      console.log(`  ${board[sq_pos]}  |  ${board[sq_pos + 1]}  |  ${board[sq_pos + 2]}`);
-    }
-    if(brdRowCtr !== BOARD_ROWS - 1) {
-      console.log('-----+-----+-----');
+  console.clear();
+  for(let brdRow = 0; brdRow < BOARD_ROWS; brdRow += 1) {
+    displaySquares(board, brdRow);
+    if(brdRow !== BOARD_ROWS - 1) {
+      console.log(Array(BOARD_COLS).fill(SEPARATOR).join('+'));
     }
   }
 }
 
 function board_initialize() {
-  let empty_board = {};
+  let empty_board = [];
   for (let ctr = 0; ctr < MAX_SQUARES; ctr += 1) {
     empty_board[ctr] = ' ';
   }
@@ -31,6 +46,7 @@ function board_initialize() {
 
 
 let board = board_initialize();
+board[1] = 'X';
 displayBoard(board);
 
 
