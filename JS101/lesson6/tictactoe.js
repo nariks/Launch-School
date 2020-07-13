7e3ab57ceb3befa6aad5a6ced601ab5653c7d114
Narayan D <happycow@gmail.com>
2020-07-13 05:53:56 -0400

    cleaned up code for scalable board

--- JS101/lesson6/tictactoe.js
+++ JS101/lesson6/tictactoe.js
@@ -1,24 +1,25 @@
 const BOARD_ROWS = 3;
 const BOARD_COLS = 3;
 const SQUARE_ROWS = 3;      //Square rows must be odd;
-const SQUARE_COLS = 5;
+const SQUARE_COLS = 9;      //Square cols must be odd;
 const MAX_SQUARES = BOARD_ROWS * BOARD_COLS;
-const CTR_MAX = MAX_SQUARES + (SQUARE_ROWS - 1);
 const SPACE_FILLER = Array(SQUARE_COLS).fill(' ').join('');
 const SEPARATOR = Array(SQUARE_COLS).fill('-').join('');
 
 function displayPositions(board, row) {
-  let start = row * BOARD_COLS;
-  let end = (row * BOARD_COLS) + BOARD_COLS;
-  let positions = board.slice(start, end);
-  positions = positions.map( position => `  ${position}  `);
-  positionRow = positions.join('|');
+  let padWidth = (SQUARE_COLS - 1) / 2;
+  let padding = Array(padWidth).fill(' ').join('');
+  let sliceStart = row * BOARD_COLS;
+  let sliceEnd = sliceStart + BOARD_COLS;
+  let positions = board.slice(sliceStart, sliceEnd);
+  positions = positions.map( position => padding + position + padding);
+  let positionRow = positions.join('|');
   console.log(positionRow);
 }
 
-function displaySquares(board, row){
-  for(let sqRow = 0; sqRow < SQUARE_ROWS; sqRow += 1) {
-    if(sqRow === (SQUARE_ROWS - 1) / 2) {
+function displaySquares(board, row) {
+  for (let sqRow = 0; sqRow < SQUARE_ROWS; sqRow += 1) {
+    if (sqRow === (SQUARE_ROWS - 1) / 2) {
       displayPositions(board, row);
       continue;
     }
@@ -28,26 +29,25 @@ function displaySquares(board, row){
 
 function displayBoard(board) {
   console.clear();
-  for(let brdRow = 0; brdRow < BOARD_ROWS; brdRow += 1) {
+  for (let brdRow = 0; brdRow < BOARD_ROWS; brdRow += 1) {
     displaySquares(board, brdRow);
-    if(brdRow !== BOARD_ROWS - 1) {
+    if (brdRow !== BOARD_ROWS - 1) {
       console.log(Array(BOARD_COLS).fill(SEPARATOR).join('+'));
     }
   }
 }
 
-function board_initialize() {
-  let empty_board = [];
+function initializeBoard() {
+  let emptyBoard = [];
   for (let ctr = 0; ctr < MAX_SQUARES; ctr += 1) {
-    empty_board[ctr] = ' ';
+    emptyBoard[ctr] = ' ';
   }
-  return empty_board;
+  return emptyBoard;
 }
 
 
-let board = board_initialize();
+let board = initializeBoard();
 board[1] = 'X';
 displayBoard(board);
-
 
 
