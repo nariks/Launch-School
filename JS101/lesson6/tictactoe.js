ed70d1109cc7a83f6519ab41b61879d17dcf91a5
Narayan D <happycow@gmail.com>
2020-07-25 05:03:23 -0400

    Minimax algo created tree

--- JS101/lesson6/tictactoe.js
+++ JS101/lesson6/tictactoe.js
@@ -2,10 +2,10 @@ const readline = require("readline-sync");
 
 const PLAYER_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
-const BOARD_ROWS = 3;
-const BOARD_COLS = 3;
-const SQUARE_ROWS = 3;      //Square rows must be odd;
-const SQUARE_COLS = 5;      //Square cols must be odd;
+const BOARD_ROWS = 3;          // Min value of 2
+const BOARD_COLS = 3;          // Min value of 2 
+const SQUARE_ROWS = 3;         //Square rows must be odd;
+const SQUARE_COLS = 5;         //Square cols must be odd;
 const MAX_SQUARES = BOARD_ROWS * BOARD_COLS;
 const SPACE_FILLER = Array(SQUARE_COLS).fill(' ').join('');
 const SEPARATOR = Array(SQUARE_COLS).fill('-').join('');
@@ -22,7 +22,6 @@ function displayPositions(board, row) {
   let positionsRow = board.slice(sliceStart, sliceEnd)
                           .map( position => padding + position + padding)
                           .join('|');
-
   console.log(positionsRow);
 }
 
@@ -81,7 +80,49 @@ function playerChoosesSquare(board) {
   return board;
 }
 
+function nextTurn(turn) {
+  return (turn === "Player") ? "Computer" : "Player";
+}
+
+function marker(turn) {
+  return (turn === "Player") ? PLAYER_MARKER : COMPUTER_MARKER;
+}
+
+function createNode(tree, depth, turn) {
+  let nodeId = tree.depth[depth].slice(-1)[0] + 1;
+  tree.depth[depth + 1] = [];
+
+  if (unfilledSquares(tree[tree.depth[depth][0]]).length === 0) return tree;
+  
+  tree.depth[depth].forEach( node => {
+    let board = tree[node].slice();
+    let emptySquares = unfilledSquares(board);
+    emptySquares.forEach( sq => {
+      let copyBoard = board.slice();
+      copyBoard[sq] = marker(turn);
+      tree[nodeId] = copyBoard.slice();
+      tree.depth[depth + 1].push(nodeId);
+      nodeId += 1;
+    });
+  });
+  turn = nextTurn(turn);
+  createNode(tree, depth + 1, turn);
+}
+
+function tree(board, turn) {
+  let tree = {depth: {}};
+  let nodeId = 0;
+  let depth = 0;
+  tree.depth[depth] = [nodeId];
+  tree[nodeId] = board.slice();
+  tree = createNode(tree, depth , turn);
+}
+  
+  
+
 function computerChoosesSquare(board) {
+  let choicetree = tree(board, 'Computer');
+  let wait = readline.question("WAIT !!!");
   let emptySquares = unfilledSquares(board);
   if (!emptySquares.length) return board;
 
