f4924febdc9b4fd2252f6d153f0d037967472513
Narayan D <happycow@gmail.com>
2020-07-28 07:50:35 -0400

    did some refactor and working on calculate node score

--- JS101/lesson6/tictactoe.js
+++ JS101/lesson6/tictactoe.js
@@ -2,8 +2,8 @@ const readline = require("readline-sync");
 
 const PLAYER_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
-const BOARD_ROWS = 3;          // Min value of 2
-const BOARD_COLS = 3;          // Min value of 2 
+const BOARD_ROWS = 2;          // Min value of 2
+const BOARD_COLS = 2;          // Min value of 2
 const SQUARE_ROWS = 3;         //Square rows must be odd;
 const SQUARE_COLS = 5;         //Square cols must be odd;
 const MAX_SQUARES = BOARD_ROWS * BOARD_COLS;
@@ -88,40 +88,53 @@ function marker(turn) {
   return (turn === "Player") ? PLAYER_MARKER : COMPUTER_MARKER;
 }
 
-function createNode(tree, depth, turn) {
-  let nodeId = tree.depth[depth].slice(-1)[0] + 1;
-  tree.depth[depth + 1] = [];
+function createNodes(tree, layer, turn) {
+  let nodeId = tree.depth[layer].slice(-1)[0] + 1;
+  tree.depth[layer + 1] = [];
 
-  if (unfilledSquares(tree[tree.depth[depth][0]]).length === 0) return tree;
-  
-  tree.depth[depth].forEach( node => {
-    let board = tree[node].slice();
+  if (unfilledSquares(tree[tree.depth[layer][0]]).length === 0) {
+    return tree;
+  }
+  tree.depth[layer].forEach( node => {
+    let board = tree[node].board.slice();
+    let tree[node].children = [];
+    let tree[node].score = undefined;
     let emptySquares = unfilledSquares(board);
+    
     emptySquares.forEach( sq => {
-      let copyBoard = board.slice();
-      copyBoard[sq] = marker(turn);
-      tree[nodeId] = copyBoard.slice();
-      tree.depth[depth + 1].push(nodeId);
+      tree[node].children = nodeId;
+      tree[nodeId].board = board.slice();
+      tree[nodeId].board[sq] = marker(turn);
+      tree[nodeId].score = undefined;
+      tree.depth[layer + 1].push(nodeId);
       nodeId += 1;
     });
   });
   turn = nextTurn(turn);
-  createNode(tree, depth + 1, turn);
+  createNodes(tree, layer + 1, turn);
 }
 
-function tree(board, turn) {
+function constructTree(board, turn) {
   let tree = {depth: {}};
   let nodeId = 0;
-  let depth = 0;
-  tree.depth[depth] = [nodeId];
-  tree[nodeId] = board.slice();
-  tree = createNode(tree, depth , turn);
+  let layer = 0;
+  let tree[nodeId] = {}
+  tree.depth[layer] = [nodeId];
+  tree[nodeId].board = board.slice();
+  tree = createNodes(tree, layer, turn);
 }
-  
-  
+
+/*function minimax(tree, turn) {
+  if (turn === 'Player') {
+    tree[0].child.forEach( childNode => {
+      tree[childNode].score
+    tree[0].score = max(children_node.scores
+  } else {
+    min(tree[0].children */
 
 function computerChoosesSquare(board) {
-  let choicetree = tree(board, 'Computer');
+  let choicetree = constructTree(board, 'Computer');
+//  let choice = calculateNodevalue
   let wait = readline.question("WAIT !!!");
   let emptySquares = unfilledSquares(board);
   if (!emptySquares.length) return board;
